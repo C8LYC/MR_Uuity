2576e29126722cac8a94ae33e960f7895b5ec339
wama <36957874+april1026@users.noreply.github.com>
2024-12-14 00:48:57 +0800

    feat: add LerpMaterialColors script to Dandelion prefab

--- Assets/Scripts/LerpMaterialColors.cs
+++ Assets/Scripts/LerpMaterialColors.cs
@@ -65,7 +65,7 @@ public class LerpMaterialColors : MonoBehaviour
             if (lerpTime >= transparencyLerpDuration)
             {
                 // Destroy the parent object after full transparency
-                Destroy(parentObject);
+                Destroy(parentObject.transform.parent.gameObject);
             }
         }
     }
